6e22f7804ca56a035d9e1ac8cd3c2c6875f55aa4
inflaton <inflaton.sg@gmail.com>
2022-03-23 17:34:23 +0800

    auth0 setup

--- Chapter12/auth0/env.bash
+++ Chapter12/auth0/env.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-TENANT=
-MGM_CLIENT_ID=
-MGM_CLIENT_SECRET=
-USER_EMAIL=
-USER_PASSWORD=
+TENANT=dev-c8-4oqyp.us.auth0.com
+MGM_CLIENT_ID=i3hjIWcfXpYUezZBzpwKyoXC3NjyxCYW
+MGM_CLIENT_SECRET=LT7LPdopTN79j8Y2e4iPZWQCyoWnEdOZOAMnD3uUw_pTp_-qYroFMcyu_z2Z8wSQ
+USER_EMAIL=inflaton.sg@gmail.com
+USER_PASSWORD=password
